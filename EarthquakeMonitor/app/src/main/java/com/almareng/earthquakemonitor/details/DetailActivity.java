7d53a4c924068a334266f7b749e48305b1608fc9
Jesus Almaral <Almaral17@gmail.com>
2015-12-27 17:18:03 -0800

    Provide up navigation to DetailActivity

--- EarthquakeMonitor/app/src/main/java/com/almareng/earthquakemonitor/details/DetailActivity.java
+++ EarthquakeMonitor/app/src/main/java/com/almareng/earthquakemonitor/details/DetailActivity.java
@@ -2,7 +2,9 @@ package com.almareng.earthquakemonitor.details;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -22,6 +24,17 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        final Toolbar detailToolbar = (Toolbar) findViewById(R.id.activity_detail_toolbar);
+
+        detailToolbar.setNavigationIcon(R.mipmap.ic_arrow_left);
+        setSupportActionBar(detailToolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         final Bundle extras = getIntent().getExtras();
         final Earthquake earthquake = extras.getParcelable(EarthquakeListActivity.EARTHQUAKE_KEY);
